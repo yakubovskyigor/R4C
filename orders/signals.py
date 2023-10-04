c89b4f8bb3f0d6433f660b3edfe93ac7c17a3e92
yakubovskyigor <igorby8881@gmail.com>
2023-10-04 15:07:21 +0200

    Add description to use .env

--- orders/signals.py
+++ orders/signals.py
@@ -1,3 +1,5 @@
+import os
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Order
@@ -10,9 +12,9 @@ def notify_available_robot(instance, **kwargs):
     orders = Order.objects.filter(robot_serial=instance.serial)
     if orders.exists():
         subject = "Робот доступен!"
-        message = 'Добрый день! Недавно вы интересовались нашим роботом модели {}, версии {}. \n' \
-                  'Этот робот теперь в наличии. Если вам подходит этот вариант - пожалуйста, свяжитесь с нами.'\
-                   .format(instance.model, instance.version)
-        from_email = "igorby8881@gmail.com"  # Замените на вашу электронную почту
+        message = "Добрый день! Недавно вы интересовались нашим роботом модели {}, версии {}. \n" \
+                  "Этот робот теперь в наличии. Если вам подходит этот вариант \n" \
+                  " - пожалуйста, свяжитесь с нами.".format(instance.model, instance.version)
+        from_email = os.getenv('EMAIL_HOST_USER')
         recipient_list = [order.customer.email for order in orders]
         send_mail(subject, message, from_email, recipient_list)
